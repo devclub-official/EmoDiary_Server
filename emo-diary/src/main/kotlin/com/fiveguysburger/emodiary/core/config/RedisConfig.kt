ddc9837447bce28b5e0a6a3197a8a078d561be3e
park <wodydtns@naver.com>
2025-05-12 17:13:41 +0900

    fix: fix security danger

--- emo-diary/src/main/kotlin/com/fiveguysburger/emodiary/core/config/RedisConfig.kt
+++ emo-diary/src/main/kotlin/com/fiveguysburger/emodiary/core/config/RedisConfig.kt
@@ -1,28 +1,59 @@
 package com.fiveguysburger.emodiary.core.config
 
+import io.lettuce.core.ClientOptions
+import io.lettuce.core.SocketOptions
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 @Configuration
 class RedisConfig {
+    @Value("\${spring.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.redis.port}")
+    private var port: Int = 0
+
+    @Value("\${spring.redis.password:}")
+    private var password: String = "" // 기본값을 빈 문자열로 설정하고 lateinit 제거
+
+    @Value("\${spring.redis.database}")
+    private var database: Int = 0
+
+    @Value("\${spring.redis.timeout}")
+    private var timeout: Long = 60000
+
     @Bean
     fun redisConnectionFactory(): LettuceConnectionFactory {
         val redisConfig =
             RedisStandaloneConfiguration().apply {
-                hostName = "emodiary.duckdns.org"
-                port = 6379
-                password = RedisPassword.of("flsdl@qkqn7")
-                database = 0
+                hostName = host
+                this.port = port
+                this.password = RedisPassword.of(password.toString())
+                this.database = database
             }
 
-        return LettuceConnectionFactory(redisConfig).apply {
-            setTimeout(60000)
-        }
+        val clientOptions =
+            ClientOptions
+                .builder()
+                .socketOptions(SocketOptions.builder().connectTimeout(Duration.ofMillis(timeout)).build())
+                .build()
+
+        val clientConfig =
+            LettuceClientConfiguration
+                .builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofMillis(timeout))
+                .build()
+
+        return LettuceConnectionFactory(redisConfig, clientConfig)
     }
 
     @Bean
