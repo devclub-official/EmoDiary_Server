90a1b43efd34da07e195f6a44ce321cdd63df875
Junghyun100 <qorwjdgus8951@gmail.com>
2025-06-23 22:22:02 +0900

    [feature]알림을 받고 로그인 한지 일주일 된 사용자가 아니라 삭제

--- emo-diary/src/main/kotlin/com/fiveguysburger/emodiary/core/service/NotificationLogService.kt
+++ emo-diary/src/main/kotlin/com/fiveguysburger/emodiary/core/service/NotificationLogService.kt
@@ -37,12 +37,6 @@ interface NotificationLogService {
     fun findByNotificationStatus(status: NotificationStatus): List<NotificationLog>
 
     /**
-     * 일주일 이상 로그인하지 않은 사용자들을 조회합니다.
-     * @return 장기 미접속 사용자 ID 목록
-     */
-    fun findInactiveUsers(): List<Int>
-
-    /**
      * 알림 발송 상태를 업데이트합니다.
      * @param id 알림 로그 ID
      * @param status 변경할 상태
